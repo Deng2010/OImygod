36f0bce27206d6e319479c605647d2bf45ea3abf
Deng Ziqian <heavenaef@outlook.com>
2026-03-08 15:51:37 +0800

    适配Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// 获取仓库名称作为 base 路径
+const repoName = 'OImygod';
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
 })
